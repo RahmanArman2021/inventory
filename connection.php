2ab168c5f5ce621ca4e92fa31cd8f20565490907
Rahman Arman <78930521+RahmanArman2021@users.noreply.github.com>
2021-02-12 00:14:32 +0430

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+  $conn = mysqli_connect("localhost","root","","Inventory");
+
+  if (!$conm) {
+    die("Connection was Errored",mysqli_connect_error());
+  }
+
+
+ ?>
---
+++ header.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <!-- Latest compiled and minified CSS -->
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="boot/css/bootstrap.min.css">
+<link rel="stylesheet" href="boot/css/myCss.css">
+<!-- jQuery library -->
+<script src="boot/js/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="boot/js/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="boot/js/bootstrap.min.js"></script>
+    <title>Inventory System</title>
+  </head>
+  <body>
+  <div class="container-fluid">
+    <div class="row" id = "top">
+        <div class="col-md-3" id="logo">
+          <img class="img-responsive" src="img/logo.jpg">
+        </div>
+        <div class="col-md-4">
+
+        </div>
+        <div class="col-md-5">
+          <h1 class="titleheader" id="companyname">خدمات نرم افزاری شرکت ناب تکنالوژی</h1>
+        </div>
+      </div>
